190cea1bd24c26f7bb8bd4ce47c961ce184a4c90
kaiquebellmont <kaiquebellmont@gmail.com.br>
2021-10-20 16:10:00 -0300

    base code ready. starting refinement

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask
-from flask_restful import Resource, Api
-
+from flask_restful import Api
+from resources.hotel_resources import Hotels, Hotel
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hotels.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
 
-class Hoteis(Resource):
-    def get(self):
-        return {"Hoteis:": "Minha lista de hoteis"}
+@app.before_first_request
+def cria_banco():
+    database.create_all()
 
 
-api.add_resource(Hoteis, '/hoteis')
-print(__name__)
+api.add_resource(Hotels, '/hotels')
+api.add_resource(Hotel, '/hotel/<string:hotel_id>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from sql_alchemy import database
+
+    database.init_app(app)
+    app.run(debug=True, port=5000)
